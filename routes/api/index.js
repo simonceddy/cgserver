7e12b009bb7be2094ae6cc4c251e3ae70afc3392
Simon Eddy <simon@simoneddy.com.au>
2023-06-13 16:17:21 +1000

    removed blog code

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const blogRouter = require('./blog');
 const pageRouter = require('./page');
 
 const apiRouter = express.Router();
 
-apiRouter.use('/blog', blogRouter);
 apiRouter.use('/page', pageRouter);
 apiRouter.get('/', (_req, res) => res.json({ message: 'welcome to the api' }));
 
