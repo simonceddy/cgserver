5a202e953d77c1442e385c9481d759120d6afa05
Simon Eddy <simon@simoneddy.com.au>
2023-06-20 14:14:49 +1000

    media updates

--- routes/media.js
+++ routes/media.js
@@ -1,13 +1,38 @@
 // media router
 const express = require('express');
+const { uploadFile, getFilePath } = require('../util');
+const respondWithFile = require('../util/respondWithFile');
 
 const mediaRouter = express.Router();
 
 mediaRouter.post('/upload', (req, res) => {
-  console.log(req.files);
-  res.json({
-    message: 'Hello File Upload'
+  // console.log(req.files);
+  if (req.files) {
+    const file = Object.values(req.files)[0];
+    if (file) {
+      const src = uploadFile(file);
+      if (src === null) {
+        return res.json({
+          message: 'An error occurred.',
+          success: false,
+        });
+      }
+      return res.json({
+        message: 'Uploaded!',
+        success: true,
+        src
+      });
+    }
+  }
+  return res.json({
+    message: 'No files detected.',
+    success: false
   });
+});
+
+mediaRouter.get('/get/:src', (req, res, next) => {
+  const fn = getFilePath(req.params.src);
+  return respondWithFile(req, res, next, fn);
 });
 
 module.exports = mediaRouter;
---
+++ util/index.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const fs = require('fs');
+const { STORAGE_DIR } = require('../consts');
+
+const uploadsDir = path.join(STORAGE_DIR, 'uploads');
+
+if (!fs.existsSync(uploadsDir) || !fs.statSync(uploadsDir).isDirectory()) {
+  fs.mkdirSync(uploadsDir);
+}
+
+/**
+ * upload a file
+ * @param {import('express-fileupload').UploadedFile} file Uploaded file
+ * @returns {string|null}
+ */
+function uploadFile(file) {
+  const filename = `${Date.now()}-${file.name}`;
+  file.mv(path.join(uploadsDir, filename));
+  return filename;
+}
+
+function getFilePath(src) {
+  const fullpath = path.join(uploadsDir, src);
+  return fullpath;
+}
+
+module.exports = {
+  uploadFile,
+  getFilePath
+};
